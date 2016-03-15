1790423ba40d29a7eaaa8291c7cc9cfc0b910913
danimurillo <moder_dani@hotmail.es>
2016-03-14 22:22:50 -0600

    Commit # 1 se diseña la parte grafica de la calculadora

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Daniel Murillo
+ * @DATE 14-03-2016
+ * 
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       CALCULADORA_DANI inst = new CALCULADORA_DANI();
+       inst.setVisible(true);
+       
+    }
+    
+}
